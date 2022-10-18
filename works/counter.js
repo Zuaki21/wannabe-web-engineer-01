4ca3ca080459fa5d436dff3ddbaaa4bc0aa74449
Zuaki <67538904+Zuaki21@users.noreply.github.com>
2022-10-18 20:38:52 +0900

    全体的な見栄えの修正

--- works/counter.js
+++ works/counter.js
@@ -2,10 +2,10 @@ let count = 0;
 const countUp = () => {
     count++;
     const countElement = document.querySelector("#count");
-    countElement.textContent = "回数:" + count;
+    countElement.textContent = "回数: " + count;
 };
 const resetCount = () => {
     count = 0;
     const countElement = document.querySelector("#count");
-    countElement.textContent = "回数:" + count;
+    countElement.textContent = "回数: " + count;
 };
